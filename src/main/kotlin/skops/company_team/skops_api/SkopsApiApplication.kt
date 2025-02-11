b3fa70d5b3b813e7d2c272843d9be639fa5d6a2e
Emiliano Luna <emilianolunac09@ciencias.unam.mx>
2025-02-11 16:50:39 -0600

    primer commit xd lol

---
+++ src/main/kotlin/skops/company_team/skops_api/SkopsApiApplication.kt
@@ -0,0 +1,11 @@
+package skops.company_team.skops_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SkopsApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<SkopsApiApplication>(*args)
+}
---
+++ src/test/kotlin/skops/company_team/skops_api/SkopsApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package skops.company_team.skops_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SkopsApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
