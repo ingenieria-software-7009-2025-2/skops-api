8af544ea5ef4bf0b3208c7283511e95f845e0f1b
LuisBerna <luis_berna@ciencias.unam.mx>
2025-03-08 22:28:12 -0600

    chore: add comments

--- src/main/kotlin/skops/company_team/skops_api/SkopsApiApplication.kt
+++ src/main/kotlin/skops/company_team/skops_api/SkopsApiApplication.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.runApplication
 
 @SpringBootApplication
 class SkopsApiApplication
-
+//Foooo
 fun main(args: Array<String>) {
 	runApplication<SkopsApiApplication>(*args)
 }
