abc6e57e50275136048acb5da138ea6ab243b3db
Emiliano Luna <emilianolunac09@ciencias.unam.mx>
2025-03-24 12:46:50 -0600

    doc: Comentarios extras en Swagger

--- src/main/kotlin/skops/company_team/skops_api/usuario/controller/body/LoginUsuarioBody.kt
+++ src/main/kotlin/skops/company_team/skops_api/usuario/controller/body/LoginUsuarioBody.kt
@@ -1,3 +1,10 @@
 package skops.company_team.skops_api.usuario.controller.body
 
-data class LoginUserBody(var mail: String, var password: String)
+import io.swagger.v3.oas.annotations.media.Schema
+
+data class LoginUserBody(
+    @Schema(description = "Correo electrónico del usuario", example = "correo@ejemplo.com")
+    var mail: String,
+
+    @Schema(description = "Contraseña del usuario", example = "contra525")
+    var password: String)
--- src/main/kotlin/skops/company_team/skops_api/usuario/controller/body/UsuarioBody.kt
+++ src/main/kotlin/skops/company_team/skops_api/usuario/controller/body/UsuarioBody.kt
@@ -1,8 +1,21 @@
 package skops.company_team.skops_api.usuario.controller.body
 
-data class UsuarioBody (val username: String = "",
-                    val mail: String = "",
-                    val password: String = "",
-                    val token: String = "",
-                    val rol: String = ""
+import io.swagger.v3.oas.annotations.media.Schema
+
+data class UsuarioBody (
+
+    @Schema(description = "Nombre de usuario", example = "Dua_Lupita54")
+    val username: String = "",
+
+    @Schema(description = "Correo electrónico del usuario", example = "correo@ejemplo.com")
+    val mail: String = "",
+
+    @Schema(description = "Contraseña del usuario", example = "contra525")
+    val password: String = "",
+
+    @Schema(hidden = true) // Oculta el campo en el Swagger
+    val token: String = "",
+
+    @Schema(hidden = true) // Oculta el campo en el Swagger
+    val rol: String = ""
 )
